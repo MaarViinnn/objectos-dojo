2e669f41971c883232b819ac04e6c64af7a083db
hescarate <hescarate@estacao004.rio>
2011-07-28 16:55:57 -0300

    adicionados novos métodos para as classes Racional e TreinoDeTDD

--- objectos-dojo-team/src/main/java/br/com/objectos/dojo/hescarate/Racional.java
+++ objectos-dojo-team/src/main/java/br/com/objectos/dojo/hescarate/Racional.java
@@ -4,6 +4,7 @@
  * Propriedade de Objectos Fábrica de Software LTDA.
  * Reprodução parcial ou total proibida.
  */
+
 package br.com.objectos.dojo.hescarate;
 
 /**
@@ -16,17 +17,48 @@ public class Racional {
 
   public Racional(int numerador, int denominador) {
 
-    this.numerador = numerador;
-    this.denominador = denominador;
+    if (denominador == 0) {
+
+      throw new IllegalArgumentException();
+
+    } else {
+
+      this.numerador = numerador;
+      this.denominador = denominador;
+
+    }
+
+  }
+
+  public Racional add(Racional numero) {
+
+    int numerador = numero.numerador;
+    int denominador = numero.denominador;
+
+    int somaNumerador = this.numerador * denominador + numerador * this.denominador;
+    int somaDenominador = this.denominador * denominador;
+    return new Racional(somaNumerador, somaDenominador);
 
   }
 
   @Override
   public String toString() {
     return numerador.toString() + "/" + denominador.toString();
+
   }
 
-  // porque fica um "x" cinza no ícone da classe? e no pacote também?
-  //
-
 }
+
+// > supporting addiction
+// > public add method que pega outro Racional como um parâmetro.
+// > para pegar o Racional, o add method não deve adicionar
+// o número racional passado para ele mesmo
+// > Isso deve criar e retornar um novo Racional que guarda a
+// soma
+// > Apesar de que os parametos da classe numerador e denominador estão no
+// escopo do seu método adicional, você pode somente acessar esse valor no
+// objeto no qual add foi invocado
+// > adicionado dois campos chamados numer demon e incializa com os valores
+// dos
+// parametros da classe
+
--- objectos-dojo-team/src/test/java/br/com/objectos/dojo/hescarate/TreinoDeTDD.java
+++ objectos-dojo-team/src/test/java/br/com/objectos/dojo/hescarate/TreinoDeTDD.java
@@ -18,16 +18,37 @@ public class TreinoDeTDD {
 
   public void objetoRacionalToStringDeveImprimirFormaFracionaria() {
 
-    Racional cincoQuintos = new Racional(3, 5);
-    assertEquals(cincoQuintos.toString(), "3/5");
+    Racional cincoQuintos = new Racional(5, 5);
+    assertEquals(cincoQuintos.toString(), "5/5");
 
   }
 
   public void qualquerObjetoRacionalToStringDeveImprimirFormaFracionaria() {
 
-    Racional umQuinto = new Racional(8, 10);
-    assertEquals(umQuinto.toString(), "8/10");
+    Racional umQuinto = new Racional(1, 5);
+    assertEquals(umQuinto.toString(), "1/5");
 
   }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void objetoRacionalNaoDeveReceberInstanciaZero() {
+
+    new Racional(100, 0);
+
+  }
+
+  public void objetoRacionalToStringDeveAdicionarCampoQueRetornaSoma() {
+
+ Racional racional = new Racional(1,2);
+ Racional racional2 = new Racional(1,3);
+    
+    racional.add(racional2); 
+    
+  }
+
+  // supportando adição
+  // definir public add method na classe Racional
+  // que pega outro Racional como parâmetro
+  // deve criar e adicionar um novo Racional que pega a soma
 
 }
